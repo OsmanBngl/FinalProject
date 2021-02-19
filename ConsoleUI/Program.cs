e0fe58efa5b083d87130c05b4844d695ad6edc71
Osman <58314047+Osman686@users.noreply.github.com>
2021-02-19 20:45:19 +0300

    19.02.2021 3

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ProductTest();
+            ProductTest();
            // CategoryTest();
 
         }
@@ -29,9 +29,9 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(50, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+ "/" +product.CategoryName);
             }
         }
     }
