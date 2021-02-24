bb59b82333e88e95bdaee31a581cd0dbaf9ac1b1
Osman <58314047+Osman686@users.noreply.github.com>
2021-02-25 00:08:03 +0300

    24.02.2021 Ders Sonu

---
+++ Business/CCS/DatabaseLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Business.CCS
+{
+    public class DatabaseLogger : ILogger
+    {
+        public void log()
+        {
+            Console.WriteLine("Veritabanına loglandı");
+        }
+    }
+
+
+}
--- Business/Constans/Messages.cs
+++ Business/Constans/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constans
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductListed = "Ürünler Listelendi";
+        public static string ProductCountOfCategoryError="Ürün sayısı 10 u geçti";
+        public static string ProductNameAlreadyExists="Aynı isimde ürün var";
+        public static string CategoryLimitExceded="Kategori limiti aşıldıgı için yeni ürün eklenemiyor";
     }
 }
